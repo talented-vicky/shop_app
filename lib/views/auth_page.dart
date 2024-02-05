1389ffee7aa9b4abef0f31ed32398ae43c080464
talented_vicky <victorotubure7@gmail.com>
2024-02-05 14:42:04 +0100

    added animation to auth page

--- lib/views/auth_page.dart
+++ lib/views/auth_page.dart
@@ -51,7 +51,7 @@ class AuthWidget extends StatefulWidget {
   State<AuthWidget> createState() => _AuthWidgetState();
 }
 
-class _AuthWidgetState extends State<AuthWidget> {
+class _AuthWidgetState extends State<AuthWidget> with TickerProviderStateMixin {
   final GlobalKey<FormState> _formkey = GlobalKey();
   // gives me access to the state of the form in this widget
 
@@ -65,18 +65,50 @@ class _AuthWidgetState extends State<AuthWidget> {
 
   bool _isLoading = false;
   final _passwordCtrl = TextEditingController();
+  late AnimationController _animeCtrl;
+  late Animation<Size> _animeHeight;
+  late Animation<double> _opacity;
+
+  @override
+  void initState() {
+    _animeCtrl = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 600),
+    );
+    _animeHeight = Tween<Size>(
+      begin: const Size(double.infinity, 260),
+      end: const Size(double.infinity, 320),
+    ).animate(CurvedAnimation(
+      parent: _animeCtrl,
+      curve: Curves.easeIn,
+    ));
+    _opacity = Tween(begin: 0.0, end: 1.0).animate(CurvedAnimation(
+      parent: _animeCtrl,
+      curve: Curves.easeIn,
+    ));
+
+    // triggering the build method so we
+    // _animeHeight.addListener(() => setState(() {}));
+    // I used animatedBuilder() so I don't need to manually
+    // call build anymore
+    super.initState();
+  }
 
   @override
   void dispose() {
     _passwordCtrl.dispose();
+    // _animeCtrl.dispose();
     super.dispose();
   }
 
   void _swithAuthState() {
     if (_authstate == AuthState.logIn) {
       setState(() => _authstate = AuthState.signUp);
+      // // now we trigger/start the animation
+      _animeCtrl.forward();
     } else {
       setState(() => _authstate = AuthState.logIn);
+      _animeCtrl.reverse();
     }
   }
 
@@ -135,84 +167,95 @@ class _AuthWidgetState extends State<AuthWidget> {
   Widget build(BuildContext context) {
     final deviceSize = MediaQuery.of(context).size;
     return Container(
-        height: _authstate == AuthState.signUp ? 320 : 260,
-        constraints: BoxConstraints(
-            minHeight: _authstate == AuthState.signUp ? 320 : 260),
-        width: deviceSize.width * .75,
-        padding: const EdgeInsets.all(12),
-        child: Form(
-            key: _formkey,
-            child: SingleChildScrollView(
-                child: Column(
-              children: [
-                TextFormField(
-                  decoration: const InputDecoration(labelText: 'E-mail'),
-                  keyboardType: TextInputType.emailAddress,
-                  validator: (val) {
-                    if (val!.isEmpty || !val.contains('@')) {
-                      return 'Invalid Email';
-                    }
-                    return null;
-                  },
-                  onSaved: (val) {
-                    _authInfo['email'] = val!;
-                  },
-                ),
-                TextFormField(
-                  obscureText: true, // asteriks thingy
-                  decoration: const InputDecoration(labelText: 'Password'),
-                  controller: _passwordCtrl,
-                  validator: (val) {
-                    if (val!.isEmpty || val.length <= 8) {
-                      return 'Password must be at least 8 char';
-                    }
-                    return null;
-                  },
-                  onSaved: (val) {
-                    _authInfo['password'] = val!;
-                  },
-                ),
-                const SizedBox(height: 10),
-                if (_authstate == AuthState.signUp)
-                  TextFormField(
-                      enabled: _authstate == AuthState.signUp,
-                      obscureText: true,
-                      decoration:
-                          const InputDecoration(labelText: 'Confrim Password'),
-                      validator: _authstate == AuthState.signUp
-                          ? (val) {
-                              if (val != _passwordCtrl.text) {
-                                return 'Passwords do not match';
-                              }
-                            }
-                          : null),
-                const SizedBox(height: 10),
-                if (_isLoading)
-                  const CircularProgressIndicator()
-                else
-                  ElevatedButton(
-                    onPressed: _signUser,
-                    child: Text(
-                        _authstate == AuthState.signUp ? 'SIGN UP' : 'LOGIN'),
-                    // style: ButtonStyle(shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30))),
-                  ),
-                const SizedBox(height: 10),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        child: AnimatedContainer(
+            duration: const Duration(milliseconds: 300),
+            curve: Curves.easeIn,
+            // animation: _animeHeight,
+            // builder: (ctxt, ch) => Container(
+            // height: _animeHeight.value.height,
+            height: _authstate == AuthState.signUp ? 320 : 260,
+            constraints: BoxConstraints(
+                // minHeight: _animeHeight.value.height),
+                minHeight: _authstate == AuthState.signUp ? 320 : 260),
+            width: deviceSize.width * .75,
+            padding: const EdgeInsets.all(12),
+            child: Form(
+                key: _formkey,
+                child: SingleChildScrollView(
+                    child: Column(
                   children: [
-                    Text(
-                        '${_authstate == AuthState.logIn ? 'Dont' : 'Already'} have an account?'),
-                    TextButton(
-                      onPressed: _swithAuthState,
-                      style: const ButtonStyle(
-                        tapTargetSize: MaterialTapTargetSize.shrinkWrap,
-                      ),
-                      child: Text(
-                          '${_authstate == AuthState.logIn ? 'SIGNUP' : 'LOGIN'} INSTEAD'),
+                    TextFormField(
+                      decoration: const InputDecoration(labelText: 'E-mail'),
+                      keyboardType: TextInputType.emailAddress,
+                      validator: (val) {
+                        if (val!.isEmpty || !val.contains('@')) {
+                          return 'Invalid Email';
+                        }
+                        return null;
+                      },
+                      onSaved: (val) {
+                        _authInfo['email'] = val!;
+                      },
                     ),
+                    TextFormField(
+                      obscureText: true, // asteriks thingy
+                      decoration: const InputDecoration(labelText: 'Password'),
+                      controller: _passwordCtrl,
+                      validator: (val) {
+                        if (val!.isEmpty || val.length <= 8) {
+                          return 'Password must be at least 8 char';
+                        }
+                        return null;
+                      },
+                      onSaved: (val) {
+                        _authInfo['password'] = val!;
+                      },
+                    ),
+                    const SizedBox(height: 10),
+                    // if (_authstate == AuthState.signUp)
+                    FadeTransition(
+                      opacity: _opacity,
+                      child: TextFormField(
+                          enabled: _authstate == AuthState.signUp,
+                          obscureText: true,
+                          decoration: const InputDecoration(
+                              labelText: 'Confrim Password'),
+                          validator: _authstate == AuthState.signUp
+                              ? (val) {
+                                  if (val != _passwordCtrl.text) {
+                                    return 'Passwords do not match';
+                                  }
+                                }
+                              : null),
+                    ),
+                    const SizedBox(height: 10),
+                    if (_isLoading)
+                      const CircularProgressIndicator()
+                    else
+                      ElevatedButton(
+                        onPressed: _signUser,
+                        child: Text(_authstate == AuthState.signUp
+                            ? 'SIGN UP'
+                            : 'LOGIN'),
+                        // style: ButtonStyle(shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30))),
+                      ),
+                    const SizedBox(height: 10),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Text(
+                            '${_authstate == AuthState.logIn ? 'Dont' : 'Already'} have an account?'),
+                        TextButton(
+                          onPressed: _swithAuthState,
+                          style: const ButtonStyle(
+                            tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                          ),
+                          child: Text(
+                              '${_authstate == AuthState.logIn ? 'SIGNUP' : 'LOGIN'} INSTEAD'),
+                        ),
+                      ],
+                    )
                   ],
-                )
-              ],
-            ))));
+                )))));
   }
 }
